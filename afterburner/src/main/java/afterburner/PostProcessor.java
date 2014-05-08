e8aaaf67cad53883574af710765e1c3fecc2cafc
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-05-08 17:44:41 +0200

    Work for content view and fragments. POC

--- afterburner/src/main/java/afterburner/PostProcessor.java
+++ afterburner/src/main/java/afterburner/PostProcessor.java
@@ -1,30 +1,101 @@
 package afterburner;
 
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+import javassist.CannotCompileException;
 import javassist.CtClass;
+import javassist.CtField;
 import javassist.CtMethod;
-import javassist.CtNewMethod;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectFragment;
+import roboguice.inject.InjectView;
 
 import com.github.drochetti.javassist.maven.ClassTransformer;
 
 public class PostProcessor extends ClassTransformer {
 
-    private String appendValue = "steff";
-
     @Override
-    protected void applyTransformations(CtClass classToTransform) throws Exception {
+    protected void applyTransformations(final CtClass classToTransform) throws Exception {
         // Actually you must test if it exists, but it's just an example...
-        CtMethod toStringMethod;
+
+        CtMethod onCreateMethod = classToTransform.getDeclaredMethod("onCreate");
+        onCreateMethod.instrument( new ExprEditor() {
+            @Override
+            public void edit(MethodCall m) throws CannotCompileException {
+                System.out.println("method call "+m.getMethodName());
+                if( m.getMethodName().equals("onCreate") ) {
+                    int layoutId = -1;
+                    try {
+                        layoutId = ((ContentView)m.getEnclosingClass().getAnnotation(ContentView.class)).value();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    List<CtField> views = getAllInjectedFieldsForAnnotation(m.getEnclosingClass(), InjectView.class);
+                    List<CtField> fragments = getAllInjectedFieldsForAnnotation(m.getEnclosingClass(), InjectFragment.class);
+                    String fieldsNameList = extractAllNames(views);
+                    System.out.println("super.onCreate call "+m.getMethodName());
+                    String string = "$_ = $proceed($$);\n"+
+                            "System.out.println(\"Injectable fields in : "+m.getEnclosingClass().getName()+"\");\n"+
+                            "System.out.println(\"fields : "+fieldsNameList+"\");\n"+
+                            "setContentView("+layoutId+");\n"+
+                            injectFragmentStatements(fragments);
+                    System.out.println("Injected : " + string);
+                    m.replace(string );
+                }
+            }
+
+
+        });
+        classToTransform.detach();
+    }
+
+    private String injectFragmentStatements(List<CtField> fragments) {
+        StringBuffer buffer = new StringBuffer();
         try {
-            toStringMethod = classToTransform.getDeclaredMethod("toString");
-            classToTransform.removeMethod(toStringMethod);
+            for( CtField field : fragments ) {
+                int id;
+                id = ((InjectFragment)field.getAnnotation(InjectFragment.class)).value();
+                buffer.append( field.getName() );
+                buffer.append( " = " );
+                buffer.append( '(' );
+                buffer.append( field.getType().getName() );
+                buffer.append( ')' );
+                buffer.append( "getFragmentManager().findFragmentById("+id+");\n" );
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return buffer.toString();
+    }
+    private String extractAllNames(List<CtField> fields) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append('[');
+        for (CtField field : fields) {
+            buffer.append(field.getName());
+            buffer.append(',');
+        }
+        if( buffer.length() > 1 ) {
+            buffer.deleteCharAt(buffer.length()-1);
+        }
+        buffer.append(']');
+        return buffer.toString();
+    }
 
-        CtMethod hackedToStringMethod = CtNewMethod.make(
-                "public String toString() { return \"toString() hacked by Javassist"+(this.appendValue  != null ? this.appendValue:"")+"\"; }",
-                classToTransform);
-        classToTransform.addMethod(hackedToStringMethod);
+    private List<CtField> getAllInjectedFieldsForAnnotation(CtClass clazz, Class<? extends Annotation> annotationClazz) {
+        List<CtField> result = new ArrayList<CtField>();
+        CtField[] allFields = clazz.getDeclaredFields();
+        System.out.println("Scanning fields in "+clazz.getName());
+        for (CtField field : allFields) {
+            System.out.println("Discovered field "+field.getName());
+            if( field.hasAnnotation(annotationClazz) ) {
+                result.add(field);
+            }
+        }
+        return result;
     }
 
 }
